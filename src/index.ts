f26b85f0a71ba4f04a1311a45b661520c519b777
dee-diaz <diazthediablo@yahoo.com>
2026-03-21 20:54:22 -0300

    feat: set up Apollo GraphQL server with initial schema

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+
+const server = new ApolloServer({
+  typeDefs,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log('Server ready at port', 4000);
