532c3b93aa3259c39aa399d5139bbee2c48e6da7
dee-diaz <diazthediablo@yahoo.com>
2026-03-21 21:21:25 -0300

    feat: add GraphQL resolvers and connect mock database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema';
+import db, { authors } from './_db';
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+
+    reviews() {
+      return db.reviews;
+    },
+
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
